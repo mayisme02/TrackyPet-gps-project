710a1060bd10a518dea8931f318c39e028ed6294
mayisme02 <chonticha.so@kkumail.com>
2025-08-18 17:43:02 +0700

    update

---
+++ app-iot-pet/app/router.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="Login" />
+      <Stack.Screen name="Register" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="Resetpassword" />
+    </Stack>
+  );
+}

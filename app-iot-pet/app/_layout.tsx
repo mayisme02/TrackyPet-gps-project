ee5089d90cfe8a25a33087897189249438d02805
mayisme02 <chonticha.so@kkumail.com>
2025-09-25 17:34:41 +0700

    delete backend

--- app-iot-pet/app/_layout.tsx
+++ app-iot-pet/app/_layout.tsx
@@ -1,14 +1,6 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import { useAuthStore } from "../store/authStore";
 
 export default function RootLayout() {
-  const { checkAuth } = useAuthStore();
-
-  useEffect(() => {
-    checkAuth();
-  }, []);
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />

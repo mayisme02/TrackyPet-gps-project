a8217e1cee6799d54212a08905a2a865886a4ba3
sroithongthae346-6 <sroithongthae.a@kkumail.com>
2026-03-21 00:27:09 +0700

    update head

--- app-iot-pet/components/ParallaxScrollView.tsx
+++ app-iot-pet/components/ParallaxScrollView.tsx
@@ -11,7 +11,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-const HEADER_HEIGHT = 120;
+const HEADER_HEIGHT = 109;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
